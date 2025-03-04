b29d86c86a08e655125b49a8bf0e897838f421af
aironspider <aironspider30@gmail.com>
2025-03-04 14:33:16 -0500

    new finally

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 class Node {
 public:
@@ -37,6 +38,25 @@ public:
 
     void reverseLinkedList() {
         // TODO: Students will implement this function
+        Node* temp = head;
+        int node_count = 0;
+        while (temp != nullptr) {
+            node_count++;
+            temp = temp->next;
+        }
+        temp = head;
+        int *datatot = new int[node_count];
+        for (int i = 0; i < node_count; i++) {
+            datatot[i] = temp->data;
+            Node* old_node = temp;
+            temp = temp->next;
+            delete[] old_node;
+        }
+        head = nullptr;
+        for (int i = node_count - 1; i >= 0; i--) {
+            append(datatot[i]);
+        }
+        delete[] datatot;
         std::cout << "Implement reverseLinkedList()" << std::endl;
     }
 };
